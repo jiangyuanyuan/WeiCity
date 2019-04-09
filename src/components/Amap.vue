95fecf170ee75e672e4f321097ee50bafa6e82b8
zhuqibin <zqbemail@126.com>
2019-04-09 19:50:32 +0800

    vue高德demo

---
+++ src/components/Amap.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="container" class="mymap"></div>
+</template>
+<script>
+
+  import AMap from 'AMap';   //在页面中引入高德地图
+
+  export default {
+    mounted(){
+      this.loadmap();     //加载地图和相关组件
+    },
+    methods: {
+      loadmap(){
+        const map = new AMap.Map('container', {
+          zoom: 9
+        });
+      }
+    }
+  }
+</script>
+<style scoped>
+  .mymap{
+    width: 300px;
+    height: 300px;
+  }
+</style>
+
